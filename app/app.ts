775e6f4f9fcc135ec62a504a37eaea1bb5121778
sarangkulkarni99 <sarang.kulkarni99@gmail.com>
2024-03-14 12:27:39 +0530

    initial commit

---
+++ app/app.ts
@@ -0,0 +1,25 @@
+import cors from 'cors';
+import express, { json } from 'express';
+import helmet from 'helmet';
+import { createServer as createHttpServer } from 'http';
+import { socketHandler } from './middlewares/socket.middleware';
+
+export const startServer = () => {
+	try {
+		const app = express();
+		const server = createHttpServer(app);
+		app.use(helmet());
+		app.use(cors());
+		app.use(json());
+
+		socketHandler(server);
+
+		const { PORT } = process.env;
+		server.listen(PORT, () => {
+			console.log(`SERVER RUNNING ON PORT: ${PORT}`);
+		});
+	} catch (error) {
+		console.error(`COULD NOT START SERVER: ${error}`);
+		process.exit(1);
+	}
+};
---
+++ app/middlewares/socket.middleware.ts
@@ -0,0 +1,52 @@
+import WebSocket from 'ws';
+import { Server as HttpServer } from 'http';
+
+// Array to store WebSocket clients
+const clients: WebSocket[] = [];
+
+// Function to handle WebSocket connections
+export const socketHandler = (server: HttpServer) => {
+	// Create a WebSocket server instance
+	const wss = new WebSocket.Server({ server });
+
+	// Event listener for new WebSocket connections
+	wss.on('connection', (ws: WebSocket) => {
+		console.log('Client connected!');
+
+		// Add the new client to the clients array
+		clients.push(ws);
+
+		// Event listener for incoming messages from clients
+		ws.on('message', (message: WebSocket.Data) => {
+			// Broadcast the received message to all clients
+			broadcastMessage(message.toString(), ws);
+		});
+
+		// Event listener for client disconnection
+		ws.on('close', () => {
+			console.log('Client disconnected!');
+			// Remove the disconnected client from the clients array
+			const index = clients.indexOf(ws);
+			if (index !== -1) {
+				clients.splice(index, 1);
+			}
+		});
+
+		// Event listener for WebSocket errors
+		ws.on('error', (error: Error) => {
+			console.error('WebSocket error:', error);
+		});
+	});
+
+	// Function to broadcast a message to all clients except the sender
+	const broadcastMessage = (message: string, sender: WebSocket) => {
+		clients.forEach((client) => {
+			if (client !== sender && client.readyState === WebSocket.OPEN) {
+				client.send(message);
+			}
+		});
+	};
+
+	// Log a message indicating that the WebSocket server has been initialized
+	console.log('WebSocket server initialized.');
+};
